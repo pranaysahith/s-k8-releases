726d7109d34a5af65f725c95369b8a0530d42434
NourEddineX <noureddinex@protonmail.com>
2020-09-10 18:54:40 +0200

    fixes

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -16,5 +16,20 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.27.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 rm VBoxGuest*.iso
+#!/bin/bash
+
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
+update-grub
+rm /etc/netplan/*.yml /etc/netplan/*.yaml
+cat > /etc/netplan/network.yaml <<EOF
+network:
+  version: 2
+  ethernets:
+    eth0:
+      dhcp4: true
+    eth1:
+      dhcp4: true
+EOF
+
 sudo apt autoclean
 sudo apt clean all
