fec6766f8d1ef53e0a42fc0d5ed98e18b0ecd7fa
NourEddineX <noureddinex@protonmail.com>
2020-09-10 14:25:12 +0200

    Packer OVA

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-SSH_USER=${SSH_USERNAME:-vagrant}
+SSH_USER=${SSH_USERNAME:-user}
+DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update && sudo apt-get install curl git -y
 
-sudo apt install docker.io -y
+curl https://get.docker.com -o /tmp/docker-installer.sh
+chmod +x /tmp/docker-installer.sh
+sudo /tmp/docker-installer.sh
 
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo systemctl enable --now docker
+sudo usermod -aG docker $1
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
-
+rm VBoxGuest*.iso
+sudo apt autoclean
+sudo apt clean all
